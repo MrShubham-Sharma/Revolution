e2cd37d5993351fc0eb7d7ba4a47519b6248c3b7
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-07-22 00:22:24 +0530

    added some line's

--- for_loop.py
+++ for_loop.py
@@ -1,5 +1,8 @@
 scores = [5.67, 5.93, 5.61, 6.22, 6.74, 7.59]
 for sgpa in scores:
     if sgpa <= 5.7:
-        continue
-    print(f"your Good pointers are {sgpa}")
+        print(f"your Good pointers are {sgpa}")
+    elif sgpa <= 6.5:
+        print(f"your sgpa {sgpa} is good keep improving")
+    elif sgpa >6:
+        print(f"Nice")
