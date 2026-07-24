5b1105cec5442ecf751e8dac4cb81dda96d8eb33
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-07-25 00:38:34 +0530

    reverse list sorting

--- list/sort_list.py
+++ list/sort_list.py
@@ -1,3 +1,8 @@
 github = ['for loop','variable','block']
 github.sort()
 print(github)
+
+#for sorting in desending use the (reverse=true)
+github = ['for loop','variable','block']
+github.sort(reverse=True)
+print(github)
