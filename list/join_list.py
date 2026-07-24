d85c089649f61c2fa3d8d22589a62303249a6d8e
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-07-25 01:06:39 +0530

    join two list by the append.() function

--- list/join_list.py
+++ list/join_list.py
@@ -3,3 +3,10 @@ list1=[25,33,4,5,55,333]
 list2=[29,35,556,35]
 list3= list1 +list2
 print(list3)
+
+#join list by the append()
+list3=[25,33,4,5,55,333]
+list4=[234,54,56,33,5]
+for x in list4:
+    list3.append(x)
+print(list3)
