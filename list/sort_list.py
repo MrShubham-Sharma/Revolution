3af30de514ffa76a6b699a1304da99a300e4b5b6
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-07-25 00:41:56 +0530

    sorting by closest number

--- list/sort_list.py
+++ list/sort_list.py
@@ -6,3 +6,12 @@ print(github)
 github = ['for loop','variable','block']
 github.sort(reverse=True)
 print(github)
+
+
+#sorting by the closest number of 50
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
