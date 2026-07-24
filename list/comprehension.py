31d979496bccb6e5102391b295b0e7df18960ff8
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-07-24 23:52:20 +0530

    comprehension with in single line

--- list/comprehension.py
+++ list/comprehension.py
@@ -1,8 +1,17 @@
+#we want to create spacific list with new list
 fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 newlist = []
 
 for x in fruits:
+# only "a" letter word will be added in new list 
   if "a" in x:
     newlist.append(x)
+
+print(newlist)
+
+#with the single line syntax
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
 
 print(newlist)
