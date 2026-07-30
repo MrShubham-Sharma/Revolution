8101ee82338de60a358561969bceb40e818665c6
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-07-30 23:34:41 +0530

    looping with items with while loop in tuple

--- Tuple/loop_on_tuple.py
+++ Tuple/loop_on_tuple.py
@@ -7,3 +7,11 @@ for x in thistuple:
 for i in range(len(thistuple)):
   count+=1
   print(f"items in the Tuple {count} Item's are {thistuple[i]} ")
+
+# Print all items, using a while loop to go through all the index numbers:
+Material = ("Stick","Wood","cement","Bricks")
+i=0
+while i < len(Material):
+  print(Material[i])
+  i = i+1
+  i=+1
