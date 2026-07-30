391c1cbb037d97a963da51a901fcca121125d27c
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-07-30 23:43:53 +0530

    Added Comment for use of count and index

--- Tuple/join_tuple.py
+++ Tuple/join_tuple.py
@@ -7,3 +7,7 @@ print(tuple3)
 # multiply tuple2
 tuple2 =tuple2*2
 print(tuple2)
+
+# count()	Returns the number of times a specified value occurs in a tuple
+
+# index()	Searches the tuple for a specified value and returns the position of where it was found
