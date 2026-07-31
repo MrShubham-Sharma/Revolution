24568be82006436edce7bd34276c9f425019747d
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-08-01 00:46:20 +0530

    Remove oprations on sets

---
+++ Sets/removeing_items_in_set.py
@@ -0,0 +1,15 @@
+Friends ={"Parth","Rohit","Shubh","Prashant"}
+Friends.remove("Prashant")
+print(Friends)
+
+Friends.add("Omkar")
+
+x = Friends.pop()
+
+Friends.discard("Omkar")
+print(Friends)
+
+Friends.clear()
+print(Friends)
+
+del Friends
