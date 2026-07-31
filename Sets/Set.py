7117ff3e99f8cf7d22f9fd1a19d32e3f1a3e46e0
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-07-31 23:58:17 +0530

    More concept

--- Sets/Set.py
+++ Sets/Set.py
@@ -1,2 +1,17 @@
 Friends = {"Rohit","Parth","Shubh","Shubh"}
+
+# in the set all items get sorted
+# double items are not printed twice only single time 
+
 print(Friends)
+
+# set can be any data type
+Set = {1,3,5,"Shubh",2.4}
+print(Set)
+
+# Create The Set By the set() finction
+Set1=set((1,2,4,4,32,5,5,3,5,6,4))
+print(Set1)
+
+# for find the type of set 
+print(type(Set1))
