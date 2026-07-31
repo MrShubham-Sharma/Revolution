cc241ab1708c6e113cfb13a956a67607c47cea4e
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-08-01 00:46:47 +0530

    Remove oprations on sets

--- Sets/removeing_items_in_set.py
+++ Sets/removeing_items_in_set.py
@@ -1,15 +1,21 @@
 Friends ={"Parth","Rohit","Shubh","Prashant"}
+
+# .remove() removes the Item from list, if item in not list it will cause error
 Friends.remove("Prashant")
 print(Friends)
 
 Friends.add("Omkar")
 
+# .pop() removes the random item 
 x = Friends.pop()
 
+# .discard() removes the Item from list, if item in not list it will NOT cause error
 Friends.discard("Omkar")
 print(Friends)
 
+# .clear() remove items from sets and gives the null set
 Friends.clear()
 print(Friends)
 
+# del Delete the whole set from existance
 del Friends
