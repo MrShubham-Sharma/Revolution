1c69b8dec9a01ad3945589aa9dc9f08bffeb16af
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-08-03 23:57:50 +0530

    Use of .items() ,.value(), .keys()

--- dictionaries/dictionaries.py
+++ dictionaries/dictionaries.py
@@ -17,3 +17,31 @@ print(Friends)
 
 print(len(Me))
 print(len(Friends))
+
+x= Friends["Parth"]
+print(x)
+
+# There is also a method called get() that will give you the same result:
+
+y= Friends.get("Rohit")
+print(y)
+
+# The keys() method will return a list of all the keys in the dictionary.
+z=Friends.keys()
+print(z)
+
+# The list of the keys is a view of the dictionary, 
+# meaning that any changes done to the dictionary will be reflected in the keys list.
+b=Friends.keys()
+Friends["om"]= 21
+
+print(b)
+B=Friends.values()
+print(B)
+
+# The returned list is a view of the items of the dictionary, 
+# meaning that any changes done to the dictionary will be reflected in the items list.
+A=Friends.items()
+print(A)
+if "Shubh" in Friends:
+    print("Yeahh !")
