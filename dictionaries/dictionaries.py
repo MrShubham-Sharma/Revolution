28293f9a96a8eca5aa2155c3ad4369d40e720347
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-08-03 23:38:25 +0530

    Dictionaries with simple oprations

---
+++ dictionaries/dictionaries.py
@@ -0,0 +1,19 @@
+
+# The values in dictionary items can be of any data type:
+Me= {
+    "Name": "Shubh",
+    "Age" : 21,
+    "Profession" :"Software Engineer",
+    "colors": ["red", "white", "blue"]
+}
+print(Me)
+print(type(Me))
+
+# The dict() Constructor
+# It is also possible to use the dict() constructor to make a dictionary.
+
+Friends = dict(Shubh=21,Parth=21,Rohit=21)
+print(Friends)
+
+print(len(Me))
+print(len(Friends))
