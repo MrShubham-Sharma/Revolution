9ee28ff6b41153d03cc63a8683def2de39f8dc34
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-08-04 00:23:46 +0530

    Use of the .pop() ,.popitem() to remove the items from the dictionaries

--- dictionaries/change_item.py
+++ dictionaries/change_item.py
@@ -1,8 +1,16 @@
 nigga ={
     "1st":"Parth","2nd":"Shubh","3rd":"Rohit"
 }
+# syntax for add the item in dictionaries
 nigga["4th"]="Pratik"
 print(nigga)
 
+# .update() used for the update dictinaries
 nigga.update({"all":"nigga"})
+print(nigga)
+
+nigga.pop("4th")
+print(nigga)
+
+nigga.popitem()
 print(nigga)
