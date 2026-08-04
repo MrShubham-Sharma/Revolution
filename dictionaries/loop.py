323ab2eadc178be8ffaee083cb8b9b2b98511ad1
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-08-04 23:49:59 +0530

    Loops using .values(), .items(), .keys()

--- dictionaries/loop.py
+++ dictionaries/loop.py
@@ -8,11 +8,17 @@ for x in car:
 
 print(car["Modle"])
 
+# Return an object providing a view on the dict's values.
+
 for x in car.values():
     print(x)
 
+# Return a set-like object providing a view on the dict's keys.
+
 for x in car.keys():
     print(x)
+
+# Return a set-like object providing a view on the dict's items.
 
 for x in car.items():
     print(x)
