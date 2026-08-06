b8cff944476ed419ea864e33e2da384229eb0815
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-08-06 23:59:56 +0530

    use of * in count

--- Functions/arg _and_kwarg.py
+++ Functions/arg _and_kwarg.py
@@ -4,3 +4,18 @@ def my_function(*Friends):
     print(type(Friends))
     print(f"all friends are {Friends}")
 my_function("Shubh","Rohit","parth")
+
+def friends(greeting,*names):
+    for name in names:
+        print(greeting,name)
+friends("Hello","Shubh","Parth","rohit")
+
+def my_function(*numbers):
+  total = 0
+  for num in numbers:
+    total += num
+  return total
+
+print(my_function(1, 2, 3))
+print(my_function(10, 20, 30, 40))
+print(my_function(5))
