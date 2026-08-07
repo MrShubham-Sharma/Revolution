a7a5ffa471ac274bdf876802d367db090d284c7a
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-08-07 23:50:26 +0530

    Min number in tuple

--- Functions/arg _and_kwarg.py
+++ Functions/arg _and_kwarg.py
@@ -32,3 +32,14 @@ def my_function(*numbers):
   return max_num
 
 print(my_function(3, 7, 2, 9, 1))
+
+def my_function(*numbers):
+    if len(numbers) == 0:
+        return None
+    min_num = numbers[0]
+    for num in numbers:
+        if num < min_num:
+            min_num = num
+    return min_num 
+
+print(my_function(5, 3, 6, 7, 8, 3))
