1fa56c082c9aff0853673a4fcf04c9dd9a540b04
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-08-09 22:55:55 +0530

    globle scope

--- Functions/scope.py
+++ Functions/scope.py
@@ -7,9 +7,19 @@ def my_function():
     print(x)
 my_function()
 
+# Function Inside Function
+
+# As explained in the example above, 
+# the variable x is not available outside the function, but it is available for any function inside the function:
 def Hello():
     greeting = "hello"
     def hello():
         print(greeting)
     hello()
 Hello()
+
+x=300
+def num():
+    print(x)
+num()
+print(x)
