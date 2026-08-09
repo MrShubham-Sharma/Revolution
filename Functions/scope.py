9d814a562e731140dca6dccac6bed3a2f007c425
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-08-09 22:59:16 +0530

    naming Variable

--- Functions/scope.py
+++ Functions/scope.py
@@ -18,8 +18,25 @@ def Hello():
     hello()
 Hello()
 
+# Global Scope
+
+# A variable created in the main body of the Python code is a global variable and belongs to the global scope.
+# Global variables are available from within any scope, global and local.
+
 x=300
 def num():
     print(x)
 num()
+print(x)
+
+# Naming Variables
+
+# If you operate with the same variable name inside and outside of a function, Python will treat them as two separate variables, 
+# one available in the global scope (outside the function) and one available in the local scope (inside the function):
+
+x = "Shubh"
+def hi():
+    x="Parth"
+    print(x)
+hi()
 print(x)
