dc7cbe88935b980708ad5085ba9df96b2c378d74
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-08-10 01:31:24 +0530

    parcatice of *args and **kwargs

--- Functions/arg _and_kwarg.py
+++ Functions/arg _and_kwarg.py
@@ -70,3 +70,32 @@ def my_function(Collage_Name,*Fees,**Student):
    print(f"total fess is {total_fees}")
    print(f"Student Data :{Student}")
 my_function("Slrtce",12000,4000,43000,name="Shubham",age=21) 
+
+def Data(**Hey):
+   User_name =Hey.get("Name")
+   User_age =Hey["age"]
+   for x,v in Hey.items():
+      print(f"{x} > {v}")
+Data(Name="Shubham",age=21,goal="SDE")
+
+def Laptop(Brand,**Specs):
+   print(f"The Laptop Brand is {Brand}")
+   print(f"Ram: is {Specs.get("ram")}")
+   print(f"The Price is {Specs.get("Price")}")
+   print("Remaning Specs are")
+   for x,v in Specs.items():
+      if x not in ["ram","Price"]:
+         print(f"{x} -> {v}")
+Laptop(Brand='lenovo',ram="16Gb",Price=74000,Storage="1TB SSD",GPU="RTX2050")
+
+Employee_data={"name":"Shubh","age":21,"job":"SDE"}
+def Employee(name,age,job):
+   print(f"Employee: {name}, he's Age is {age} and He's an {job}")
+Employee(**Employee_data)
+
+def passenger(*passengers):
+   for p in passengers:
+      print(f"passengers Name is {p.get("name")} and he's seat is {p.get("Seat")}")
+p1={"name" : "SHUBHAM" ,"Seat":"B3"}
+p2={"name" : "PARTH" ,"Seat":"B6"}
+passenger(p1,p2)
