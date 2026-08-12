c0c449ebcadf107f38c16c5dfb797b59b6e8eb8d
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-08-12 23:41:55 +0530

    using map() lambda function

--- Functions/Lambda.py
+++ Functions/Lambda.py
@@ -21,3 +21,19 @@ triple = my_function(3)
 double =my_function(2)
 print(triple(22))
 print(double(220))
+
+# Use lambda functions when an anonymous function is required for a short period of time.
+
+# Lambda with Built-in Functions
+
+# Lambda functions are commonly used with built-in functions like map(), filter(), and sorted().
+# Using Lambda with map()
+# The map() function applies a function to every item in an iterable:
+
+list1 = [1,2,4,4,64,3]
+triple = list(map(lambda a:a *3, list1))
+print(triple)
+
+list2 =[1,4,56,6,3,5]
+double= list(map(lambda a,b :a*b , list1 ,list2))
+print(double)
